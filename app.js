e2a0c5e0d681adc78cd5ce1da978fc9345a805d1
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2024-12-13 10:52:09 -0500

    Taking Inputs from user [name] | welcome message | selecting game level

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const btnPlay =  document.createElement('button');
 
 let scoreCounter = 0;
 let liveCounter = 4;
+let gameLevel = 4;
 let allGameCards = [1,2,3,4,5,6,7,8,9,10];
 //Loading Page and initialization
 window.onload = () => {
@@ -24,7 +25,13 @@ startButton.addEventListener('click',
     (event) => {
             resetGameCounters();
             let playerName = prompt('Welcome to Cards Valley! \nPlease Enter Your Name');        
-            alert(`Welcome ${playerName}!\nNew Game Just Started\nEnjoy it!`);
+            // alert(`Welcome ${playerName}!\nNew Game Just Started\nEnjoy it!`);
+            alert(
+                `Welcome ${playerName}!
+                New Game Just Started
+                Enjoy it!`); 
+                            // Enter Number of cards between 4 and 10`);        
+            showGameLevelSelection();
     }
 );
 
@@ -52,7 +59,7 @@ allGameCards.forEach((element)=> {
     
     const cardBackSide =  document.createElement('div');
     cardBackSide.classList.add('cardBackSide');
-    cardBackSide.innerHTML ='<h2>Card Back</h2>'
+    cardBackSide.innerHTML =`<h2>${element}</h2>`
     
     
     //adding cards to interface
@@ -76,8 +83,34 @@ allGameCards.forEach((element)=> {
 
     const h1Flip = document.createElement('div');
     h1Flip.classList.add('h1Flip');
-
+    h1Flip.innerHTML = '<h1>12</h1>';
+    
     randomFlashCardContainer.appendChild(h1Flip);
     randomCard.appendChild(randomFlashCardContainer);
     gameBoard.appendChild(randomCard);
 }
+
+//game level display
+function showGameLevelSelection() {
+    document.getElementById("gameLevelsSelection").style.display = "block";
+  }
+
+  function handleGameLevel() {
+    
+    // if(! document.querySelector('input[name="option"]:checked').value) alert('xx');
+    const selectedLevel = document.querySelector('input[name="option"]:checked').value ;
+    
+    console.log("Selected Level:" + selectedLevel);
+    alert("Selected Level:" + selectedLevel);
+    // gameLevel = selectedLevel;
+    var ele = document.getElementsByName("option");
+    for(var i=0;i<ele.length;i++)
+        {
+            if(ele[i].value == 4)
+                ele[i].checked = true;
+            else
+                ele[i].checked = false;}
+    // // document.querySelector('input[name="option"]:checked');
+    document.querySelector('input[name="option"]:checked').value = 4;
+    document.getElementById("gameLevelsSelection").style.display = "none";
+  }
