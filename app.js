e23144876a3cd0538e2453fd8a20efa3e03ee52c
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2024-12-13 09:06:22 -0500

    Ammending styles and layout

--- app.js
+++ app.js
@@ -4,25 +4,26 @@ const liveCount = document.querySelector('.liveCount');
 let scoreCounter = 0;
 let liveCounter = 4;
 window.onload = () => {
-    document.title = '*** SBA 316 ***';
+    document.title = '*** Cards Valley - SBA 316 ***';
     resetGameCounters();
 };
 
 const btnPlay =  document.createElement('button');
+btnPlay.classList.add('btnNewGame');
 btnPlay.textContent = 'Start Game';
 
 startButton.addEventListener('click',
     (event) => {
-            resetGameCounters();        
+            resetGameCounters();
+            let playerName = prompt('Welcome to Cards Valley! \nPlease Enter Your Name');        
+            alert(`Welcome ${playerName}!\nNew Game Just Started\nEnjoy it!`);
     }
 );
 startButton.appendChild(btnPlay);
 
 
 function resetGameCounters(){
-
     scoreCount.innerHTML = 'Score: ' + scoreCounter ;
     liveCount.innerHTML = 'Lives: ' +  liveCounter;
-
 
 }
